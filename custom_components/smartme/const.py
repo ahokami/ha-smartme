6b071805a4ad4dd3b5503549e7f4fde35f34ab96
assymba <service@assymba.ch>
2025-01-03 14:16:39 +0100

    Fixing usage of end of life classes

--- custom_components/smartme/const.py
+++ custom_components/smartme/const.py
@@ -1,15 +1,13 @@
 """Constants for the Smart-me integration."""
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+)
 from homeassistant.const import (
-    DEVICE_CLASS_ENERGY,
-    DEVICE_CLASS_POWER,
-    DEVICE_CLASS_VOLTAGE,
-    DEVICE_CLASS_CURRENT,
-    DEVICE_CLASS_TIMESTAMP,
-    ENERGY_KILO_WATT_HOUR,
-    POWER_KILO_WATT,
-    ELECTRIC_POTENTIAL_VOLT,
-    ELECTRIC_CURRENT_AMPERE,
     PERCENTAGE,
+    UnitOfElectricCurrent,
+    UnitOfElectricPotential,
+    UnitOfEnergy,
+    UnitOfPower,
 )
 
 DOMAIN = "smartme"
@@ -20,62 +18,62 @@ SENSOR_TYPES = {
     "ActivePower": {
         "key": "ActivePower",
         "name": "Active Power",
-        "device_class": DEVICE_CLASS_ENERGY,
-        "unit": POWER_KILO_WATT,
+        "device_class": SensorDeviceClass.POWER,
+        "unit": UnitOfPower.KILO_WATT,
     },
     "ActivePowerL1": {
         "key": "ActivePowerL1",
         "name": "Active Power L1",
-        "device_class": DEVICE_CLASS_ENERGY,
-        "unit": POWER_KILO_WATT,
+        "device_class": SensorDeviceClass.POWER,
+        "unit": UnitOfPower.KILO_WATT,
     },
     "ActivePowerL2": {
         "key": "ActivePowerL2",
         "name": "Active Power L2",
-        "device_class": DEVICE_CLASS_ENERGY,
-        "unit": POWER_KILO_WATT,
+        "device_class": SensorDeviceClass.POWER,
+        "unit": UnitOfPower.KILO_WATT,
     },
     "ActivePowerL3": {
         "key": "ActivePowerL3",
         "name": "Active Power L3",
-        "device_class": DEVICE_CLASS_ENERGY,
-        "unit": POWER_KILO_WATT,
+        "device_class": SensorDeviceClass.POWER,
+        "unit": UnitOfPower.KILO_WATT,
     },
     
     # Energy readings
     "CounterReading": {
         "key": "CounterReading",
         "name": "Counter Reading",
-        "device_class": DEVICE_CLASS_ENERGY,
-        "unit": ENERGY_KILO_WATT_HOUR,
+        "device_class": SensorDeviceClass.ENERGY,
+        "unit": UnitOfEnergy.KILO_WATT_HOUR,
         "state_class": "total_increasing",
     },
     "CounterReadingT1": {
         "key": "CounterReadingT1",
         "name": "Counter Reading T1",
-        "device_class": DEVICE_CLASS_ENERGY,
-        "unit": ENERGY_KILO_WATT_HOUR,
+        "device_class": SensorDeviceClass.ENERGY,
+        "unit": UnitOfEnergy.KILO_WATT_HOUR,
         "state_class": "total_increasing",
     },
     "CounterReadingT2": {
         "key": "CounterReadingT2",
         "name": "Counter Reading T2",
-        "device_class": DEVICE_CLASS_ENERGY,
-        "unit": ENERGY_KILO_WATT_HOUR,
+        "device_class": SensorDeviceClass.ENERGY,
+        "unit": UnitOfEnergy.KILO_WATT_HOUR,
         "state_class": "total_increasing",
     },
     "CounterReadingImport": {
         "key": "CounterReadingImport",
         "name": "Counter Reading Import",
-        "device_class": DEVICE_CLASS_ENERGY,
-        "unit": ENERGY_KILO_WATT_HOUR,
+        "device_class": SensorDeviceClass.ENERGY,
+        "unit": UnitOfEnergy.KILO_WATT_HOUR,
         "state_class": "total_increasing",
     },
     "CounterReadingExport": {
         "key": "CounterReadingExport",
         "name": "Counter Reading Export",
-        "device_class": DEVICE_CLASS_ENERGY,
-        "unit": ENERGY_KILO_WATT_HOUR,
+        "device_class": SensorDeviceClass.ENERGY,
+        "unit": UnitOfEnergy.KILO_WATT_HOUR,
         "state_class": "total_increasing",
     },
     
@@ -83,46 +81,46 @@ SENSOR_TYPES = {
     "Voltage": {
         "key": "Voltage",
         "name": "Voltage",
-        "device_class": DEVICE_CLASS_VOLTAGE,
-        "unit": ELECTRIC_POTENTIAL_VOLT,
+        "device_class": SensorDeviceClass.VOLTAGE,
+        "unit": UnitOfElectricPotential.VOLT,
     },
     "VoltageL1": {
         "key": "VoltageL1",
         "name": "Voltage L1",
-        "device_class": DEVICE_CLASS_VOLTAGE,
-        "unit": ELECTRIC_POTENTIAL_VOLT,
+        "device_class": SensorDeviceClass.VOLTAGE,
+        "unit": UnitOfElectricPotential.VOLT,
     },
     "VoltageL2": {
         "key": "VoltageL2",
         "name": "Voltage L2",
-        "device_class": DEVICE_CLASS_VOLTAGE,
-        "unit": ELECTRIC_POTENTIAL_VOLT,
+        "device_class": SensorDeviceClass.VOLTAGE,
+        "unit": UnitOfElectricPotential.VOLT,
     },
     "VoltageL3": {
         "key": "VoltageL3",
         "name": "Voltage L3",
-        "device_class": DEVICE_CLASS_VOLTAGE,
-        "unit": ELECTRIC_POTENTIAL_VOLT,
+        "device_class": SensorDeviceClass.VOLTAGE,
+        "unit": UnitOfElectricPotential.VOLT,
     },
     
     # Current measurements
     "CurrentL1": {
         "key": "CurrentL1",
         "name": "Current L1",
-        "device_class": DEVICE_CLASS_CURRENT,
-        "unit": ELECTRIC_CURRENT_AMPERE,
+        "device_class": SensorDeviceClass.CURRENT,
+        "unit": UnitOfElectricCurrent.AMPERE,
     },
     "CurrentL2": {
         "key": "CurrentL2",
         "name": "Current L2",
-        "device_class": DEVICE_CLASS_CURRENT,
-        "unit": ELECTRIC_CURRENT_AMPERE,
+        "device_class": SensorDeviceClass.CURRENT,
+        "unit": UnitOfElectricCurrent.AMPERE,
     },
     "CurrentL3": {
         "key": "CurrentL3",
         "name": "Current L3",
-        "device_class": DEVICE_CLASS_CURRENT,
-        "unit": ELECTRIC_CURRENT_AMPERE,
+        "device_class": SensorDeviceClass.CURRENT,
+        "unit": UnitOfElectricCurrent.AMPERE,
     },
     
     # Power factor measurements
@@ -157,6 +155,6 @@ SENSOR_TYPES = {
     "ValueDate": {
         "key": "ValueDate",
         "name": "Last Update",
-        "device_class": DEVICE_CLASS_TIMESTAMP,
+        "device_class": SensorDeviceClass.TIMESTAMP,
     },
 }
